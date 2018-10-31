797d2c24741f243b1a24d2d2755c0cd68fc093d3
sipp11 <sipp11@zzyzx.co>
2018-10-31 12:17:16 +0900

    Add Pelias endpoints

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -29,7 +29,29 @@ const Query = {
       // console.log(error);
       return []
     }
-  }
+  },
+  async poi_search(_, { text }) {
+    const url = `https://poi.everyday.in.th/v1/search`
+    try {
+      const params = { text: text }
+      const resp = await axios.get(url, { params });
+      console.log(resp.data.features)
+      return resp.data.features;
+    } catch (error) {
+      return []
+    }
+  },
+  async reverse_geo(_, {lon, lat}) {
+    const url = `https://poi.everyday.in.th/v1/reverse`
+    try {
+      const params = { 'point.lon': lon, 'point.lat': lat }
+      const resp = await axios.get(url, { params });
+      console.log(resp.data)
+      return resp.data.features;
+    } catch (error) {
+      return []
+    }
+  },
 }
 
 module.exports = Query;
