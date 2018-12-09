b59480cc0dd7fc35903f144c6d39f81752d214ea
sipp11 <sipp11@zzyzx.co>
2018-12-10 02:05:18 +0900

    Dealing with CORS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+// const cors = require('cors');
 
 require("dotenv").config({ path: ".env" });
 const createServer = require("./createServer");
 const server = createServer();
 const db = require("./db");
 
+// var whitelist = ['http://localhost:3000', 'http://localhost:4000']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     var ind = whitelist.indexOf(origin)
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log(whitelist, ind, whitelist[ind])
+//       callback(whitelist[ind])
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+//   credentials: true,
+// }
+
 server.express.use(cookieParser());
+// .use(cors(corsOptions));
 // decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
@@ -34,8 +50,8 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: "*" // process.env.FRONTEND_URL,
-    }
+      origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    },
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
