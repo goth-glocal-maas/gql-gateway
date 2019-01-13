4e493d7b1e1950eb523efd953d5de324fefcf9f1
sipp11 <sipp11@zzyzx.co>
2019-01-13 17:01:07 +0900

    POI search with bound

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
-const { hasPermission } = require('../utils');
+const axios = require("axios");
+const { hasPermission } = require("../utils");
 // const { forwardTo } = require('prisma-binding');
 
 const Query = {
   hello: (_, { name }) => ({
-    message: `Hello ${name || 'World'}`,
-    name,
+    message: `Hello ${name || "World"}`,
+    name
   }),
   // users: forwardTo('db'),
   me(parent, args, ctx, info) {
@@ -15,7 +15,7 @@ const Query = {
     }
     return ctx.db.query.user(
       {
-        where: { id: ctx.request.userId },
+        where: { id: ctx.request.userId }
       },
       info
     );
@@ -23,15 +23,15 @@ const Query = {
   async users(parent, args, ctx, info) {
     // 1. Check if they are logged in
     if (!ctx.request.userId) {
-      throw new Error('You must be logged in!');
+      throw new Error("You must be logged in!");
     }
     // 2. Check if the user has the permissions to query all the users
-    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
 
     // 2. if they do, query all the users!
     return ctx.db.query.users({}, info);
   },
-  async route_plan(_, {from, to, date, time, arriveBy, mode}) {
+  async route_plan(_, { from, to, date, time, arriveBy, mode }) {
     const url = `https://otp.goth.app/otp/routers/default/plan`;
     try {
       const params = {
@@ -39,11 +39,11 @@ const Query = {
         toPlace: to,
         date,
         time,
-        mode: mode || 'WALK,TRANSIT',
+        mode: mode || "WALK,TRANSIT",
         arriveBy: arriveBy || false,
         wheelchair: false,
         maxWalkDistance: 5000,
-        locale: 'en'
+        locale: "en"
       };
       //console.log(params)
       const resp = await axios.get(url, { params });
@@ -51,73 +51,87 @@ const Query = {
       return resp.data.plan;
     } catch (error) {
       // console.log(error);
-      return []
+      return [];
     }
   },
-  async poi_search(_, { text }) {
-    const url = `https://poi.everyday.in.th/v1/search`
+  async poi_search(_, { text, lat, lon, province }) {
+    const url = `https://poi.everyday.in.th/v1/search`;
     try {
-      const params = { text: text }
+      //
+      let params = { text: text };
+      if (lat && lon) {
+        params = { ...params, "focus.point.lat": lat, "focus.point.lon": lon };
+      }
+      if (province.lower() === "phuket") {
+        params = {
+          ...params,
+          "boundary.rect.min_lon": 98.24344235287894,
+          "boundary.rect.max_lon": 98.46442465791307,
+          "boundary.rect.min_lat": 7.746297072966113,
+          "boundary.rect.max_lat": 8.22605833195936
+        };
+      }
+      // console.log(params);
       const resp = await axios.get(url, { params });
-      console.log(resp.data.features)
+      // console.log(resp.data.features);
       return resp.data.features;
     } catch (error) {
-      return []
+      return [];
     }
   },
-  async reverse_geo(_, {lon, lat}) {
-    const url = `https://poi.everyday.in.th/v1/reverse`
+  async reverse_geo(_, { lon, lat }) {
+    const url = `https://poi.everyday.in.th/v1/reverse`;
     try {
-      const params = { 'point.lon': lon, 'point.lat': lat }
+      const params = { "point.lon": lon, "point.lat": lat };
       const resp = await axios.get(url, { params });
-      console.log(resp.data)
+      // console.log(resp.data);
       return resp.data.features;
     } catch (error) {
-      return []
+      return [];
     }
   },
   async stops(_, { min_lat, min_lon, max_lat, max_lon }) {
-    const url = `https://otp.goth.app/otp/routers/default/index/stops`
+    const url = `https://otp.goth.app/otp/routers/default/index/stops`;
     try {
       const params = {
         minLat: min_lat,
         minLon: min_lon,
         maxLat: max_lat,
-        maxLon: max_lon,
-      }
-      const resp = await axios.get(url, { params })
-      return resp.data
+        maxLon: max_lon
+      };
+      const resp = await axios.get(url, { params });
+      return resp.data;
     } catch (error) {
-      return []
+      return [];
     }
   },
   async stop_detail(_, { stop_id }) {
-    const url = `https://otp.goth.app/otp/routers/default/index/stops/${stop_id}`
+    const url = `https://otp.goth.app/otp/routers/default/index/stops/${stop_id}`;
     try {
-      const resp = await axios.get(url)
-      return resp.data
+      const resp = await axios.get(url);
+      return resp.data;
     } catch (error) {
-      return []
+      return [];
     }
   },
   async stop_route(_, { stop_id }) {
-    const url = `https://otp.goth.app/otp/routers/default/index/stops/${stop_id}/routes`
+    const url = `https://otp.goth.app/otp/routers/default/index/stops/${stop_id}/routes`;
     try {
-      const resp = await axios.get(url)
-      return resp.data
+      const resp = await axios.get(url);
+      return resp.data;
     } catch (error) {
-      return []
+      return [];
     }
   },
   async stop_stoptimes(_, { stop_id }) {
-    const url = `https://otp.goth.app/otp/routers/default/index/stops/${stop_id}/stoptimes`
+    const url = `https://otp.goth.app/otp/routers/default/index/stops/${stop_id}/stoptimes`;
     try {
-      const resp = await axios.get(url)
-      return resp.data
+      const resp = await axios.get(url);
+      return resp.data;
     } catch (error) {
-      return []
+      return [];
     }
-  },
-}
+  }
+};
 
 module.exports = Query;
